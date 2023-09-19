ea052d768ffafe4d42ec6b9208c3f0f46729103f
local <local02070@gmail.com>
2023-09-19 18:04:56 +0900

    회원가입 유효성검사, 회원정보수정 유효성 검사 추가, 상태코드로 응답 추가예정

--- src/main/java/com/local/blog/dto/ResponseDto.java
+++ src/main/java/com/local/blog/dto/ResponseDto.java
@@ -6,11 +6,19 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 
+import java.util.Map;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class ResponseDto<T> {
-    int status;
+    private int status;
+    private String message;
     T data;
+
+    public ResponseDto(int status, T data) {
+        this.status = status;
+        this.data = data;
+    }
 }
